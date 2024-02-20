2eba70b941ae2866a8da05b33b3bf8af37da3029
bgordon8 <gordonbrian16@gmail.com>
2024-02-20 14:54:20 -0600

    adds express sanity check

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res, next) => {
+  res.status(200).json({ success: true, message: 'hello world' });
+});
+
 app.listen(3000, () => {
   console.log('listening on port:3000...');
 });
